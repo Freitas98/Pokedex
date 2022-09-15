fb2ed53c0704d6e1319352193b8f7c83fdc26c01
Baptista <joaobfreitas09@gmail.com>
2022-09-15 15:27:22 +0100

    added PokemonCard layout, added pokemon page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
-import Head from 'next/head'
 import Image from 'next/image'
+import PokemonCard from '../components/PokemonCard';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251;
+
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const rest = await fetch(`${api}/?limit=${maxPokemons}`)
+
+  const data = await rest.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+}
+
+export default function Home( {pokemons} ) {
   return (
     <>
-      <h1>Pokedex</h1>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>
+          Poke<span>Dex</span>
+        </h1>
+        <Image 
+          src="/images/pokeball.png"
+          width="50" 
+          height="50"
+          alt="Pokeball"
+        />
+      </div>
+      <div className={styles.pokemonContainer}>
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
     </>
   )
 }
