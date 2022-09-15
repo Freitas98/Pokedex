44d983cf5145928d535a6064a3bd67d14a0cf141
Baptista <joaobfreitas09@gmail.com>
2022-09-15 01:49:24 +0100

    added layout, added footer and navbar

---
+++ components/Navbar.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Image src="/images/pokeball.png" width="50px" height="50px"/>
+                <h1>Pokedex</h1>
+            </div>
+            <ul className={styles.link_items}>
+                <li>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>   
+                <li>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
